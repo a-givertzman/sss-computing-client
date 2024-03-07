863660ececdabc337ea0af141124215fffe486b9
nyannet <nyaneet@gmail.com>
2024-03-07 16:29:46 +0300

    add BarChart widget to StrengthPage

--- lib/presentation/strength/strength_page.dart
+++ lib/presentation/strength/strength_page.dart
@@ -1,5 +1,8 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:hmi_core/hmi_core_app_settings.dart';
+import 'package:sss_computing_client/presentation/strength/widgets/bar_chart/bar_chart.dart';
+import 'package:sss_computing_client/presentation/strength/widgets/bar_chart/chart_axis.dart';
 
 class StrengthPage extends StatelessWidget {
   const StrengthPage({super.key});
@@ -23,9 +26,55 @@ class StrengthPage extends StatelessWidget {
                   child: Card(
                     margin: EdgeInsets.zero,
                     child: Padding(
-                      padding:
-                          EdgeInsets.all(const Setting('padding').toDouble),
-                      child: const Text('Shear forces [BarChart]'),
+                      padding: EdgeInsets.all(
+                        const Setting('padding').toDouble,
+                      ),
+                      child: BarChart(
+                        caption: 'Shear Force',
+                        barColor: Colors.lightGreenAccent,
+                        minX: -100.0,
+                        maxX: 100.0,
+                        minY: -200.0,
+                        maxY: 200.0,
+                        xAxis: ChartAxis(
+                          valueInterval: 25,
+                          labelsSpaceReserved: 55.0,
+                          captionSpaceReserved: 0.0,
+                          isLabelsVisible: false,
+                        ),
+                        yAxis: ChartAxis(
+                          valueInterval: 50,
+                          labelsSpaceReserved: 60.0,
+                          captionSpaceReserved: 15.0,
+                          caption: '[kN]',
+                        ),
+                        stream: Stream<Map<String, dynamic>>.periodic(
+                          const Duration(seconds: 5),
+                          (_) {
+                            final (min, max) = (-100, 100);
+                            return {
+                              'yValues': List.generate(
+                                4,
+                                (_) =>
+                                    min +
+                                    Random().nextInt(max - min).toDouble(),
+                              ),
+                              'xOffsets': [
+                                (-100.0, -50.0),
+                                (-50.0, 0.0),
+                                (0.0, 50.0),
+                                (50.0, 100.0),
+                              ],
+                              'lowLimits': [-50.0, -75.0, -100.0, -75.0],
+                              'highLimits': [50.0, 75.0, 100.0, 75.0],
+                              'barCaptions': List.generate(
+                                4,
+                                (idx) => 'Frames [${idx * 2}-${(idx + 1) * 2}]',
+                              ),
+                            };
+                          },
+                        ),
+                      ),
                     ),
                   ),
                 ),
@@ -37,9 +86,55 @@ class StrengthPage extends StatelessWidget {
                   child: Card(
                     margin: EdgeInsets.zero,
                     child: Padding(
-                      padding:
-                          EdgeInsets.all(const Setting('padding').toDouble),
-                      child: const Text('Bending moments [BarChart]'),
+                      padding: EdgeInsets.all(
+                        const Setting('padding').toDouble,
+                      ),
+                      child: BarChart(
+                        caption: 'Bending Moment',
+                        barColor: Colors.lightGreenAccent,
+                        minX: -100.0,
+                        maxX: 100.0,
+                        minY: -500.0,
+                        maxY: 500.0,
+                        xAxis: ChartAxis(
+                          valueInterval: 25,
+                          labelsSpaceReserved: 55.0,
+                          captionSpaceReserved: 0.0,
+                          isLabelsVisible: false,
+                        ),
+                        yAxis: ChartAxis(
+                          valueInterval: 100,
+                          labelsSpaceReserved: 60.0,
+                          captionSpaceReserved: 15.0,
+                          caption: '[kNm]',
+                        ),
+                        stream: Stream<Map<String, dynamic>>.periodic(
+                          const Duration(seconds: 5),
+                          (_) {
+                            final (min, max) = (-250, 250);
+                            return {
+                              'yValues': List.generate(
+                                4,
+                                (_) =>
+                                    min +
+                                    Random().nextInt(max - min).toDouble(),
+                              ),
+                              'xOffsets': [
+                                (-100.0, -50.0),
+                                (-50.0, 0.0),
+                                (0.0, 50.0),
+                                (50.0, 100.0),
+                              ],
+                              'lowLimits': [-250.0, -250.0, -250.0, -250.0],
+                              'highLimits': [200.0, 250.0, 250.0, 200.0],
+                              'barCaptions': List.generate(
+                                4,
+                                (idx) => 'Frames [${idx * 2}-${(idx + 1) * 2}]',
+                              ),
+                            };
+                          },
+                        ),
+                      ),
                     ),
                   ),
                 ),
