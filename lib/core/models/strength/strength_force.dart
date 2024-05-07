5f84701b82d6279cb5b9450bfc4d8690c65c9b2d
nyaneet <nyaneet@gmail.com>
2024-05-07 21:49:39 +0300

    add StrengthForce model

---
+++ lib/core/models/strength/strength_force.dart
@@ -0,0 +1,43 @@
+///
+/// Common data for corresponding [StrengthForce].
+abstract interface class StrengthForce {
+  /// id of the ship for corresponding [StrengthForce]
+  int get shipId;
+  /// id of the project for corresponding [StrengthForce]
+  int? get projectId;
+  /// index of the frame for corresponding [StrengthForce]
+  int get frameIndex;
+  /// value of [StrengthForce]
+  double? get value;
+  /// lower limit for value of [StrengthForce]
+  double get lowLimit;
+  /// upper limit for value of [StrengthForce]
+  double get highLimit;
+}
+///
+/// [StrengthForce] that parses itself from json map.
+final class JsonStrengthForce implements StrengthForce {
+  final Map<String, dynamic> _json;
+  /// [StrengthForce] that parses itself from json map.
+  const JsonStrengthForce({
+    required Map<String, dynamic> json,
+  }) : _json = json;
+  //
+  @override
+  int get shipId => _json['shipId'];
+  //
+  @override
+  int? get projectId => _json['projectId'];
+  //
+  @override
+  int get frameIndex => _json['frameIndex'];
+  //
+  @override
+  double? get value => _json['value'];
+  //
+  @override
+  double get lowLimit => _json['lowLimit'];
+  //
+  @override
+  double get highLimit => _json['highLimit'];
+}
