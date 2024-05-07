6973e85349fb587570953a1eb3c7c9905466a00a
nyaneet <nyaneet@gmail.com>
2024-05-07 20:39:58 +0300

    remove unused import

--- lib/presentation/strength/widgets/bar_chart/chart_lines.dart
+++ lib/presentation/strength/widgets/bar_chart/chart_lines.dart
@@ -1,6 +1,5 @@
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
-import 'package:hmi_core/hmi_core.dart';
 ///
 class ChartLines extends StatelessWidget {
   final double _minX;
