73857ccbffbc44ac365d31c11f8c11a29c67d32e
nyaneet <nyaneet@gmail.com>
2024-05-15 19:18:38 +0300

    refactor: replace lazy loading of Frame for StrengthForce with join statement in sql
    requests for lazy loading cannot be processed in parallel on the server side and waste too much time

--- lib/core/models/strength/strength_forces.dart
+++ lib/core/models/strength/strength_forces.dart
@@ -1,7 +1,7 @@
 import 'package:ext_rw/ext_rw.dart';
 import 'package:hmi_core/hmi_core.dart' hide Result;
 import 'package:hmi_core/hmi_core_result_new.dart';
-import 'package:sss_computing_client/core/models/frame/frames.dart';
+import 'package:sss_computing_client/core/models/frame/frame.dart';
 import 'package:sss_computing_client/core/models/strength/strength_force.dart';
 ///
 /// Interface for controlling collection of [StrengthForces].
@@ -32,24 +32,35 @@ class PgShearForces implements StrengthForces {
       database: _dbName,
       sqlBuilder: (_, __) => Sql(
         sql: """
-            SELECT
-              project_id AS "projectId",
-              ship_id AS "shipId",
-              index AS "frameIndex",
-              value_shear_force / 1000.0 AS value
-            FROM strength_result
+            SELECT DISTINCT
+              sr.project_id AS "projectId",
+              sr.ship_id AS "shipId",
+              sr.index AS "frameIndex",
+              cf.value AS "frameX",
+              sr.value_shear_force / 1000.0 AS value
+            FROM strength_result AS sr
+            INNER JOIN computed_frame AS cf
+            ON
+              (sr.index = cf.index AND sr.ship_id = cf.ship_id AND cf.key = 'start_x')
+            OR
+              (sr.index = cf.index + 1 AND sr.ship_id = cf.ship_id AND cf.key = 'end_x')
             ORDER BY "frameIndex";
             """,
       ),
-      entryBuilder: (row) => row,
+      entryBuilder: (row) => JsonStrengthForce(
+        json: row
+          ..['frame'] = JsonFrame(json: {
+            'projectId': row['projectId'],
+            'shipId': row['shipId'],
+            'index': row['frameIndex'],
+            'x': row['frameX'],
+          })
+          ..['lowLimit'] = -250.0
+          ..['highLimit'] = 250.0,
+      ),
     );
     return switch (await sqlAccess.fetch()) {
-      Ok(value: final rows) => await _mapRowsToValues(
-          rows,
-          _apiAddress,
-          _dbName,
-          _authToken,
-        ),
+      Ok(value: final rows) => Ok(rows),
       Err(:final error) => Err(
           Failure<String>(
             message: '$error',
@@ -82,24 +93,35 @@ class PgBendingMoments implements StrengthForces {
       database: _dbName,
       sqlBuilder: (_, __) => Sql(
         sql: """
-            SELECT
-              project_id AS "projectId",
-              ship_id AS "shipId",
-              index AS "frameIndex",
-              value_bending_moment / 1000.0 AS value
-            FROM strength_result
+            SELECT DISTINCT
+              sr.project_id AS "projectId",
+              sr.ship_id AS "shipId",
+              sr.index AS "frameIndex",
+              cf.value AS "frameX",
+              sr.value_bending_moment / 1000.0 AS value
+            FROM strength_result AS sr
+            INNER JOIN computed_frame AS cf
+            ON
+              (sr.index = cf.index AND sr.ship_id = cf.ship_id AND cf.key = 'start_x')
+            OR
+              (sr.index = cf.index + 1 AND sr.ship_id = cf.ship_id AND cf.key = 'end_x')
             ORDER BY "frameIndex";
             """,
       ),
-      entryBuilder: (row) => row,
+      entryBuilder: (row) => JsonStrengthForce(
+        json: row
+          ..['frame'] = JsonFrame(json: {
+            'projectId': row['projectId'],
+            'shipId': row['shipId'],
+            'index': row['frameIndex'],
+            'x': row['frameX'],
+          })
+          ..['lowLimit'] = -1000.0
+          ..['highLimit'] = 1000.0,
+      ),
     );
     return switch (await sqlAccess.fetch()) {
-      Ok(value: final rows) => await _mapRowsToValues(
-          rows,
-          _apiAddress,
-          _dbName,
-          _authToken,
-        ),
+      Ok(value: final rows) => Ok(rows),
       Err(:final error) => Err(
           Failure<String>(
             message: '$error',
@@ -107,68 +129,5 @@ class PgBendingMoments implements StrengthForces {
           ),
         ),
     };
-  }
-}
-///
-Future<Result<List<StrengthForce>, Failure<String>>> _mapRowsToValues(
-  List<Map<String, dynamic>> rows,
-  ApiAddress apiAddress,
-  String dbName,
-  String? authToken,
-) async {
-  try {
-    final values = List<StrengthForce>.empty(growable: true);
-    for (final row in rows) {
-      final int frameIndex = row['frameIndex'];
-      switch (await PgFramesTheoretical(
-        dbName: dbName,
-        apiAddress: apiAddress,
-        authToken: authToken,
-      ).fetchByIndex(frameIndex)) {
-        case Ok(value: final frame):
-          values.add(JsonStrengthForce(
-            json: row
-              ..['frame'] = frame
-              ..['lowLimit'] = -500.0
-              ..['highLimit'] = 500.0,
-          ));
-          break;
-        case Err(:final error):
-          return Err(Failure<String>(
-            message: '$error',
-            stackTrace: StackTrace.current,
-          ));
-      }
-    }
-    // final frames = await Future.wait(rows.map((row) {
-    //   return PgFramesTheoretical(
-    //     dbName: dbName,
-    //     apiAddress: apiAddress,
-    //     authToken: authToken,
-    //   ).fetchByIndex(row['frameIndex']);
-    // }));
-    // for (int i = 0; i < rows.length; i++) {
-    //   switch (frames[i]) {
-    //     case Ok(value: final frame):
-    //       values.add(JsonStrengthForce(
-    //         json: rows[i]
-    //           ..['frame'] = frame
-    //           ..['lowLimit'] = -500.0
-    //           ..['highLimit'] = 500.0,
-    //       ));
-    //       break;
-    //     case Err(:final error):
-    //       return Err(Failure<String>(
-    //         message: '$error',
-    //         stackTrace: StackTrace.current,
-    //       ));
-    //   }
-    // }
-    return Ok(values);
-  } catch (error) {
-    return Err(Failure<String>(
-      message: '$error',
-      stackTrace: StackTrace.current,
-    ));
   }
 }
