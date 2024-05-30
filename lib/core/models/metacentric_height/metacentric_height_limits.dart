70a5a78887bc6d11b1803db45a7bc3cadd1a5934
nyaneet <nyaneet@gmail.com>
2024-05-30 23:35:13 +0300

    feat: add models for metacentric height

---
+++ lib/core/models/metacentric_height/metacentric_height_limits.dart
@@ -0,0 +1,9 @@
+import 'package:hmi_core/hmi_core.dart' hide Result;
+import 'package:hmi_core/hmi_core_result_new.dart';
+import 'package:sss_computing_client/core/models/metacentric_height/metacentric_height_limit.dart';
+///
+/// Interface for controlling collection of [MetacentricHeightLimits].
+abstract interface class MetacentricHeightLimits {
+  /// Get all [MetacentricHeightLimit] in [MetacentricHeightLimits] collection.
+  Future<Result<List<MetacentricHeightLimit>, Failure<String>>> fetchAll();
+}
