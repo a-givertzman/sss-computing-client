6f469a28369d97020233752c5178ce77e0349f16
nyaneet <nyaneet@gmail.com>
2024-05-30 23:32:47 +0300

    feat: add filter support for FieldRecord .fetch()

--- lib/core/models/field_record/field_record.dart
+++ lib/core/models/field_record/field_record.dart
@@ -2,43 +2,67 @@ import 'package:hmi_core/hmi_core.dart';
 import 'package:ext_rw/ext_rw.dart';
 import 'package:hmi_core/hmi_core_result_new.dart';
 ///
+/// Gives access to field of record stored in database.
 class FieldRecord<T> {
-  final String _tableName;
-  final String _field;
   final String _dbName;
+  final String _tableName;
+  final String _fieldName;
   final ApiAddress _apiAddress;
   final String? _authToken;
-  final T Function(String) _parseValue;
+  final T Function(String) _toValue;
   ///
+  /// Create [FieldRecord] that giving access
+  /// to field of record stored in database.
+  ///
+  /// Value can be obtained using:
+  ///   - `databaseName` - name of the database;
+  ///   - `tableName` - name of the database table;
+  ///   - `fieldName` - name of the table field (column);
+  ///   - `apiAddress` - [ApiAddress] of server that interact with database;
+  ///   - `authToken` - string  authentication token for accessing server;
+  ///   - `toValue` - function for parsing string representation of
+  /// field into value of desired type.
   const FieldRecord({
-    required String tableName,
-    required String field,
     required String dbName,
+    required String tableName,
+    required String fieldName,
     required ApiAddress apiAddress,
     String? authToken,
-    required T Function(String value) parseValue,
-  })  : _field = field,
+    required T Function(String value) toValue,
+  })  : _fieldName = fieldName,
         _tableName = tableName,
         _dbName = dbName,
         _apiAddress = apiAddress,
         _authToken = authToken,
-        _parseValue = parseValue;
+        _toValue = toValue;
   ///
-  Future<ResultF<T>> fetch() async {
+  Future<ResultF<T>> fetch({Map<String, dynamic>? filter}) async {
+    final filterQuery = filter?.entries
+        .map(
+          (entry) => switch (entry.value) {
+            num _ => '${entry.key} = ${entry.value}',
+            _ => "${entry.key} = '${entry.value}'"
+          },
+        )
+        .join(' ');
     final sqlAccess = SqlAccess(
       address: _apiAddress,
       authToken: _authToken ?? '',
       database: _dbName,
       sqlBuilder: (_, __) => Sql(
-        sql: 'SELECT "$_field" FROM "$_tableName" LIMIT 1;',
+        sql: """
+          SELECT "$_fieldName" FROM "$_tableName"
+          ${filterQuery != null ? 'WHERE $filterQuery' : ''}
+          LIMIT 1;
+        """,
       ),
-      entryBuilder: (row) => '${row[_field]}',
+      entryBuilder: (row) => '${row[_fieldName]}',
     );
     return sqlAccess
         .fetch()
         .then<ResultF<T>>(
           (result) => switch (result) {
-            Ok(:final value) => Ok(_parseValue(value.first)),
+            Ok(:final value) => Ok(_toValue(value.first)),
             Err(:final error) => Err(
                 Failure(
                   message: '$error',
