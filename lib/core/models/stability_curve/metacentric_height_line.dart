203df555b086feb6db75909cb94ec8fdf10592ef
nyaneet <nyaneet@gmail.com>
2024-06-27 19:30:17 +0300

    doc: add doc for MetacentricHeightLine

--- lib/core/models/stability_curve/metacentric_height_line.dart
+++ lib/core/models/stability_curve/metacentric_height_line.dart
@@ -4,14 +4,25 @@ import 'package:hmi_core/hmi_core.dart' hide Result;
 import 'package:hmi_core/hmi_core_result_new.dart';
 import 'package:sss_computing_client/core/models/stability_curve/curve.dart';
 import 'package:vector_math/vector_math_64.dart';
+
 ///
+/// [Curve] object for metacentric height. Line connecting two point
+/// on plane with righting arm as y-axis and angle of heel as x-axis.
+/// Starting point is (theta0, 0.0), where theta0 is initial static heel angle.
+/// Ending point is (theta0 + 1 radian, h), where h is corrected metacentric height (GM).
 final class MetacentricHeightLine implements Curve {
   final double _minX;
   final double _maxX;
   final double _theta0;
   final double _h;
   final double _valueInterval;
+
   ///
+  /// Creates a metacentric height [Curve] as maximum possible line segment
+  /// with starting point (`theta0`, 0,0) and ending point (`theta0` + 1 radian, `h`),
+  /// limited in x by interval [`minX`, `maxX`]. `valueInterval` - x interval
+  /// between two adjacent points of the curve (x interval between last two points
+  /// of the curve may be less than this value).
   const MetacentricHeightLine({
     required double minX,
     required double maxX,
