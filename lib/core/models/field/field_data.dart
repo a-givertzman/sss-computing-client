8efee0fb0ff2d30820f1b7efa3d180214e5f8b11
nyaneet <nyaneet@gmail.com>
2024-09-09 19:13:07 +0300

    chore: remove unused constructor from FieldData

--- lib/core/models/field/field_data.dart
+++ lib/core/models/field/field_data.dart
@@ -9,14 +9,14 @@ class FieldData<T> {
   final String id;
   final String label;
   T _initialValue;
-  final T Function(String text) toValue;
-  final String Function(T value) toText;
+  final T Function(String text) _toValue;
+  final String Function(T value) _toText;
   final ValueRecord<T> _record;
   final TextEditingController _controller;
   final Validator? _validator;
   bool _isSynced;
   ///
-  /// Model that holds data for [TextFormField] or [TextField].
+  /// Create object that holds data for [TextFormField] or [TextField].
   ///
   ///   - [label] - text with which the target field will be labeled.
   ///   - [initialValue] - initial content of the target field. Also will be set to [value].
@@ -24,33 +24,19 @@ class FieldData<T> {
   FieldData({
     required this.id,
     required this.label,
-    required this.toValue,
-    required this.toText,
+    required T Function(String) toValue,
+    required String Function(T) toText,
     required T initialValue,
     required ValueRecord<T> record,
     Validator? validator,
     bool isSynced = false,
-  })  : _initialValue = initialValue,
+  })  : _toValue = toValue,
+        _toText = toText,
+        _initialValue = initialValue,
         _record = record,
         _validator = validator,
         _isSynced = isSynced,
         _controller = TextEditingController(text: toText(initialValue));
-  ///
-  FieldData._({
-    required this.id,
-    required this.label,
-    required this.toValue,
-    required this.toText,
-    required T initialValue,
-    required ValueRecord<T> record,
-    required TextEditingController controller,
-    required Validator? validator,
-    required bool isPersisted,
-  })  : _record = record,
-        _initialValue = initialValue,
-        _controller = controller,
-        _validator = validator,
-        _isSynced = isPersisted;
   ///
   /// Initial content of the target field.
   T get initialValue => _initialValue;
@@ -59,18 +45,25 @@ class FieldData<T> {
   TextEditingController get controller => _controller;
   ///
   /// Whether content of the target changed or not.
-  bool get isChanged => toText(_initialValue) != _controller.text;
+  bool get isChanged => _toText(_initialValue) != _controller.text;
   ///
-  /// Whether content of the target synced with source or not
+  /// Whether content of the target synced with source or not.
   bool get isSynced => _isSynced;
   ///
+  /// Returns validator for its [FieldData].
   Validator? get validator => _validator;
   ///
-  /// Pull data from the database through provided [record].
+  /// Parses text to `value`.
+  T toValue(String text) => _toValue(text);
+  ///
+  /// Returns string representation of `value`.
+  String toText(T value) => _toText(value);
+  ///
+  /// Pull data from the database through provided `record`.
   Future<ResultF<T>> fetch() async {
     switch (await _record.fetch()) {
       case Ok(:final value):
-        refreshWith(toText(value));
+        refreshWith(_toText(value));
         _isSynced = true;
         return Ok(value);
       case final Err<T, Failure> err:
@@ -79,32 +72,32 @@ class FieldData<T> {
     }
   }
   ///
-  /// Persist data to the database through provided [record].
+  /// Persist data to the database through provided `record`.
   Future<ResultF<T>> save() async {
     final text = controller.text;
     switch (await _record.persist(text)) {
       case Ok():
         refreshWith(text);
         _isSynced = true;
-        return Ok(toValue(text));
+        return Ok(_toValue(text));
       case final Err<void, Failure> err:
         Log('$runtimeType | fetch').error(err.error);
         return Err<T, Failure>(err.error);
     }
   }
   ///
-  /// Sets initialaValue and controller value to provided one.
+  /// Sets `initialaValue` and `controller` text to provided one.
   void refreshWith(String text) {
     _controller.text = text;
-    _initialValue = toValue(text);
+    _initialValue = _toValue(text);
   }
   ///
-  /// Set current [value] to its [initialState].
+  /// Set current `value` to its initial state.
   void cancel() {
-    _controller.text = toText(_initialValue);
+    _controller.text = _toText(_initialValue);
   }
   ///
-  /// Returns copy of [FieldData]
+  /// Returns copy of [FieldData] with provided values.
   FieldData<T> copyWith({
     T? initialValue,
     ValueRecord<T>? record,
@@ -114,8 +107,8 @@ class FieldData<T> {
       FieldData<T>(
         id: id,
         label: label,
-        toValue: toValue,
-        toText: toText,
+        toValue: _toValue,
+        toText: _toText,
         initialValue: initialValue ?? _initialValue,
         record: record ?? _record,
         validator: validator ?? _validator,
