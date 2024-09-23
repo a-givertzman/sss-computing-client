ff1668b2ba1b5225d945a749747e4ad06334c0f9
nyaneet <nyaneet@gmail.com>
2024-09-23 18:56:24 +0300

    doc: update documentation for CargoType

--- lib/core/models/cargo/cargo_type.dart
+++ lib/core/models/cargo/cargo_type.dart
@@ -1,18 +1,34 @@
 import 'package:flutter/material.dart';
 ///
-/// Enum of cargo types with colors and labels.
+/// Enum of [Cargo] types with colors and labels.
 enum CargoType {
+  ///
+  /// "water ballast" in OCX:liquidCargoType
   ballast('Ballast tank', Colors.green, 'ballast_tank'),
+  ///
+  /// oils (e.g. vegetable oil, fuel oil) in OCX:liquidCargoType
   oilsAndFuels('Oils and fuels', Colors.brown, 'fuel_tank'),
+  ///
+  /// "fresh water" in OCX:liquidCargoType
   freshWater('Fresh water', Colors.blue, 'fresh_drinking_water_tank'),
+  ///
+  /// "ammonia" in OCX:liquidCargoType
   acidsAndAlkalis('Acids and alkalis', Colors.purple, 'urea_tank'),
+  ///
+  /// TODO: determine type based on the OCX standard
   pollutedLiquids('Polluted liquids', Colors.black, 'sundry_tank'),
+  ///
+  /// "general" in OCX:bulkCargoType
   bulkCargo('Bulk cargo', Colors.grey, 'bulk_cargo'),
+  ///
+  /// "grain" in OCX:bulkCargoType
   bulkCargoShiftable(
     'Bulk cargo, shiftable',
     Colors.grey,
     'bulk_cargo_shiftable',
   ),
+  ///
+  /// "unspecified" in OCX:unitCargoType
   other('Other', Colors.grey, 'other');
   ///
   /// Text label for cargo type
@@ -23,7 +39,7 @@ enum CargoType {
   ///
   /// Unique String identifier for cargo type
   final String key;
-  ///
+  //
   const CargoType(this.label, this.color, this.key);
   ///
   /// Creates [CargoType] from passed string representation.
