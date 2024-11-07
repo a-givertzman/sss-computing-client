c42ecabb1c8c9b360e6aa0aa008be5f0ca0a106c
nyaneet <nyaneet@gmail.com>
2024-11-07 23:46:10 +0300

    feat(SchemeFigure): add onDoubleTap and onSecondaryTap callbacks

--- lib/core/widgets/scheme/scheme_figure.dart
+++ lib/core/widgets/scheme/scheme_figure.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:sss_computing_client/core/models/figure/figure.dart';
 import 'package:sss_computing_client/core/models/figure/figure_plane.dart';
 ///
@@ -9,46 +10,58 @@ class SchemeFigure extends StatelessWidget {
   final Figure _figure;
   final Matrix4 _layoutTransform;
   final void Function()? _onTap;
+  final void Function()? _onDoubleTap;
+  final void Function()? _onSecondaryTap;
   ///
   /// Render figure on scheme. Can handle figure tap
   /// if corresponding callback has passed.
   ///
-  /// [plane] – [FigurePlane] on which figure will be rendred.
-  /// [figure] – which will be rendered.
-  /// [layoutTransform] – transformation of layout on which figure
+  /// * [plane] – [FigurePlane] on which figure will be rendered.
+  /// * [figure] - [Figure] that will be rendered.
+  /// * [layoutTransform] – transformation of layout on which figure
   /// will be rendered.
-  /// [onTap] – callback for handling taps on figure.
+  /// * [onTap] – callback for handling taps on figure.
+  /// * [onDoubleTap] – callback for handling double taps on figure.
+  /// * [onSecondaryTap] – callback for handling secondary taps on figure.
   const SchemeFigure({
     super.key,
     required FigurePlane plane,
     required Figure figure,
     required Matrix4 layoutTransform,
     void Function()? onTap,
+    void Function()? onDoubleTap,
+    void Function()? onSecondaryTap,
   })  : _plane = plane,
         _figure = figure,
         _layoutTransform = layoutTransform,
-        _onTap = onTap;
+        _onTap = onTap,
+        _onDoubleTap = onDoubleTap,
+        _onSecondaryTap = onSecondaryTap;
   //
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       behavior: HitTestBehavior.deferToChild,
       onTap: _onTap,
+      onDoubleTap: _onDoubleTap,
+      onSecondaryTap: _onSecondaryTap,
       child: MouseRegion(
         hitTestBehavior: HitTestBehavior.deferToChild,
-        cursor: _onTap != null ? SystemMouseCursors.click : MouseCursor.defer,
+        cursor: _isInteractive ? SystemMouseCursors.click : MouseCursor.defer,
         child: CustomPaint(
           painter: _SchemeFigurePainter(
             figure: _figure,
             plane: _plane,
             transform: _layoutTransform,
-            isInteractive: _onTap != null,
+            isInteractive: _isInteractive,
           ),
           willChange: true,
         ),
       ),
     );
   }
+  bool get _isInteractive =>
+      _onTap != null || _onDoubleTap != null || _onSecondaryTap != null;
 }
 ///
 class _SchemeFigurePainter extends CustomPainter {
