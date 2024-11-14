32d461989a2f783cd075aaf1c87d6d4f095d151b
InvalcreInc <152579264+InvalcreInc@users.noreply.github.com>
2024-11-14 13:57:36 +0300

    switched to new hmi_core result

--- lib/core/models/accordion/accordion_items_builder.dart
+++ lib/core/models/accordion/accordion_items_builder.dart
@@ -2,16 +2,22 @@ import 'package:flutter/foundation.dart';
 import 'package:sss_computing_client/core/models/directory/directory_info.dart';
 import 'package:sss_computing_client/core/widgets/accordion/accordion_item.dart';
 
-///
+/// A builder for creating [AccordionItem]s
 abstract interface class AccordionItems<T, A> {
-  ///
+  /// How deep to go into subdirectories
   int get deep;
+
+  /// List of items
   List<T> get items;
+
+  /// Creates [AccordionItem]s
   List<AccordionItem<A>> build();
+
   @protected
   AccordionItem<A> createFrom(T item, {int parents = 0});
 }
 
+/// Creates [AccordionItem]s from [AssetsDirectoryInfo]s
 class AssetsAccordions
     implements AccordionItems<AssetsDirectoryInfo, List<String>> {
   @override
