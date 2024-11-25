ab50483f24d9925a8454f741967153bdd45c2ce1
nyaneet <nyaneet@gmail.com>
2024-11-25 14:59:42 +0300

    update docs download script to match current structure of documentation repository

--- .github/workflows/clone_ui_doc.sh
+++ .github/workflows/clone_ui_doc.sh
@@ -9,6 +9,12 @@
 
 git submodule update --init --depth=1 -- assets/info
 git -C assets/info config core.sparseCheckout true
-mkdir -p .git/modules/assets/info/info && \
-    echo -e '!/*\n/docs/\n/README.md' >> .git/modules/assets/info/info/sparse-checkout
+
+config_dir=".git/modules/assets/info/info"
+
+mkdir -p $config_dir && \
+    echo -e '!/*' > "$config_dir/sparse-checkout" && \
+    echo -e '/docs/' >> "$config_dir/sparse-checkout" && \
+    echo -e '/reference/' >> "$config_dir/sparse-checkout" && \
+    echo -e '/assets/image/' >> "$config_dir/sparse-checkout"
 git submodule update --init --force --checkout -- assets/info
