4e3897ac76088c3110aea055d105320c904ecb0a
nyaneet <nyaneet@gmail.com>
2024-12-16 18:23:43 +0300

    docs(CopyOnWriteTextFile): update documentation for constructor and methods

--- lib/core/copy_on_write_text_file.dart
+++ lib/core/copy_on_write_text_file.dart
@@ -6,24 +6,34 @@ class CopyOnWriteTextFile implements TextFile {
   final TextFile _copySource;
   ///
   /// Creates copy-on-write [TextFile] that interact with [source]
-  /// until first write.
+  /// until [target] has not been written yet. On write updates [target]
+  /// content and lefts [source] unchanged.
   ///
-  /// On first write [source] is copied to [target]
-  /// and all interactions will be redirected to [target].
+  /// Unlike widely known mechanism,
+  /// it does not control direct writing to the [source].
   const CopyOnWriteTextFile({
     required TextFile target,
     required TextFile source,
   })  : _copyTarget = target,
         _copySource = source;
-  //
+  ///
+  /// Returns [Ok] with content of [source] if [target] has not been written yet,
+  /// and [target] content otherwise.
+  ///
+  /// Returns [Err] if no one of [target] and [source] has been written yet.
   @override
-  Future<ResultF<String>> get content async {
-    return _copyTarget.content.then((result) => result.mapOr(
-          _copySource.content,
-          (targetContent) => Ok(targetContent),
-        ));
+  Future<ResultF<String>> get content {
+    return _copyTarget.content.then(
+      (result) => result.mapOr(
+        _copySource.content,
+        (targetContent) => Ok(targetContent),
+      ),
+    );
   }
-  //
+  ///
+  /// Rewrite the entire [target] with [content].
   @override
-  Future<void> write(String content) => _copyTarget.write(content);
+  Future<void> write(String content) {
+    return _copyTarget.write(content);
+  }
 }
